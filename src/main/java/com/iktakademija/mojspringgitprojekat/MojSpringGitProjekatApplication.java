19095b878bd762f33eb7191b2a2321850caae1cb
Ana Savic Belic <savicana18.03@gmail.com>
2021-07-12 14:24:40 +0200

    initial commit sts project

---
+++ src/main/java/com/iktakademija/mojspringgitprojekat/MojSpringGitProjekatApplication.java
@@ -0,0 +1,13 @@
+package com.iktakademija.mojspringgitprojekat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MojSpringGitProjekatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MojSpringGitProjekatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iktakademija/mojspringgitprojekat/MojSpringGitProjekatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iktakademija.mojspringgitprojekat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MojSpringGitProjekatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
